1590fce2107438ca14d5204351a28271da613050
Matt Wittmann <mcwitt@gmail.com>
2020-08-18 14:47:19 -0700

    Remove outliers, skip analysis if fewer than required works

--- covid_moonshot/__init__.py
+++ covid_moonshot/__init__.py
@@ -159,12 +159,38 @@ def extract_works(
     return [r for r in results if r is not None]
 
 
+def _mask_outliers(a: np.array, max_value: float, n_devs: float):
+    """Returns a boolean array masking values that are more than
+    `n_devs` standard deviations from the mean or larger in magnitude
+    than `max_value`.
+
+    Parameters
+    ----------
+    a : array
+    max_value : float
+        Values larger (in magnitude) than this will be discarded
+    n_devs : float
+        Values farther than this number of standard deviations from
+        the mean will be discarded
+
+    Returns
+    -------
+    array of bool
+        Boolean array of same shape as `a`, with False elements
+        marking outliers in `a` and all other elements True.
+    """
+    return (np.abs(a) < max_value) & (np.abs(a - np.mean(a)) < n_devs * np.std(a))
+
+
 def analyze_phase(
     project_path: str,
     run: int,
     num_works_expected: int,
     num_steps_expected: int,
     cache_dir: Optional[str],
+    max_work_value=1e4,
+    max_n_devs=5,
+    min_num_work_values=10,
 ) -> PhaseAnalysis:
 
     paths = list_results(project_path, run)
@@ -182,14 +208,24 @@ def analyze_phase(
     f_works = np.array([w.forward_work for w in works])
     r_works = np.array([w.reverse_work for w in works])
 
-    # TODO remove outliers
+    # remove pairs of works when either is determined to be an outlier
+    f_good = _mask_outliers(f_works, max_value=max_work_value, n_devs=max_n_devs)
+    r_good = _mask_outliers(r_works, max_value=max_work_value, n_devs=max_n_devs)
+    both_good = f_good & r_good
+    f_works_good = f_works[both_good]
+    r_works_good = r_works[both_good]
+    num_work_values = both_good.sum()
 
-    # TODO remove gens with fewer than the minimum number of samples
+    if num_work_values < min_num_work_values:
+        raise ValueError(
+            f"Need at least {min_num_work_values} good work values for analysis, "
+            f"but got {num_work_values}"
+        )
 
-    delta_f, ddelta_f = pymbar.BAR(f_works, r_works)
+    delta_f, ddelta_f = pymbar.BAR(f_works_good, r_works_good)
 
     return PhaseAnalysis(
-        delta_f=delta_f, ddelta_f=ddelta_f, num_work_values=len(f_works)  # XXX
+        delta_f=delta_f, ddelta_f=ddelta_f, num_work_values=num_work_values
     )
 
 
