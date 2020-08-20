98a1e5c326e0c14e0a41f6f88152d183dd98b65e
Matt Wittmann <mcwitt@gmail.com>
2020-08-20 09:20:08 -0700

    Apply Hannah's updates

--- covid_moonshot/analysis/structures.py
+++ covid_moonshot/analysis/structures.py
@@ -182,54 +182,60 @@ def slice_snapshot(snapshot, project, run):
     """
     # Load hybrid topology file
     import numpy as np
+    import os
 
-    htf = np.load(
-        f"/home/server/server2/projects/{project}/RUNS/RUN{run}/htf.npz",
-        allow_pickle=True,
-    )["arr_0"].tolist()
+    path = f"/home/server/server2/projects/{project}/RUNS/RUN{run}"
+    if not os.path.exists(f"{path}/extracted_atomids.pi"):
+        htf = np.load(f"{path}/htf.npz", allow_pickle=True)["arr_0"].tolist()
+        # Determine mapping between hybrid topology and stored atoms in the positions.xtc
+        # <xtcAtoms v="solute"/> eliminates waters
+        nonwater_atom_indices = htf.hybrid_topology.select("not water")
+        hybrid_to_stored_map = {
+            nonwater_atom_indices[index]: index
+            for index in range(len(nonwater_atom_indices))
+        }
 
-    # Determine mapping between hybrid topology and stored atoms in the positions.xtc
-    # <xtcAtoms v="solute"/> eliminates waters
-    nonwater_atom_indices = htf.hybrid_topology.select("not water")
-    hybrid_to_stored_map = {
-        nonwater_atom_indices[index]: index
-        for index in range(len(nonwater_atom_indices))
-    }
+        # Get all atom indices from the hybrid system
+        protein_atom_indices = htf.hybrid_topology.select("protein")
+        hybrid_ligand_atom_indices = htf.hybrid_topology.select("resn MOL")
+        # Identify atom index subsets for the old and new ligands from the hybrid system
+        old_ligand_atom_indices = [
+            index
+            for index in hybrid_ligand_atom_indices
+            if index in htf._old_to_hybrid_map.values()
+        ]
+        new_ligand_atom_indices = [
+            index
+            for index in hybrid_ligand_atom_indices
+            if index in htf._new_to_hybrid_map.values()
+        ]
 
-    # Get all atom indices from the hybrid system
-    protein_atom_indices = htf.hybrid_topology.select("protein")
-    hybrid_ligand_atom_indices = htf.hybrid_topology.select("resn MOL")
-    # Identify atom index subsets for the old and new ligands from the hybrid system
-    old_ligand_atom_indices = [
-        index
-        for index in hybrid_ligand_atom_indices
-        if index in htf._old_to_hybrid_map.values()
-    ]
-    new_ligand_atom_indices = [
-        index
-        for index in hybrid_ligand_atom_indices
-        if index in htf._new_to_hybrid_map.values()
-    ]
+        # Compute sliced atom indices using atom indices within positions.xtc
+        stored_atom_indices = dict()
+        stored_atom_indices["protein"] = [
+            hybrid_to_stored_map[index] for index in protein_atom_indices
+        ]
+        stored_atom_indices["old_ligand"] = [
+            hybrid_to_stored_map[index] for index in old_ligand_atom_indices
+        ]
+        stored_atom_indices["new_ligand"] = [
+            hybrid_to_stored_map[index] for index in new_ligand_atom_indices
+        ]
+        stored_atom_indices["old_complex"] = [
+            hybrid_to_stored_map[index]
+            for index in list(protein_atom_indices) + list(old_ligand_atom_indices)
+        ]
+        stored_atom_indices["new_complex"] = [
+            hybrid_to_stored_map[index]
+            for index in list(protein_atom_indices) + list(new_ligand_atom_indices)
+        ]
 
-    # Compute sliced atom indices using atom indices within positions.xtc
-    stored_atom_indices = dict()
-    stored_atom_indices["protein"] = [
-        hybrid_to_stored_map[index] for index in protein_atom_indices
-    ]
-    stored_atom_indices["old_ligand"] = [
-        hybrid_to_stored_map[index] for index in old_ligand_atom_indices
-    ]
-    stored_atom_indices["new_ligand"] = [
-        hybrid_to_stored_map[index] for index in new_ligand_atom_indices
-    ]
-    stored_atom_indices["old_complex"] = [
-        hybrid_to_stored_map[index]
-        for index in list(protein_atom_indices) + list(old_ligand_atom_indices)
-    ]
-    stored_atom_indices["new_complex"] = [
-        hybrid_to_stored_map[index]
-        for index in list(protein_atom_indices) + list(new_ligand_atom_indices)
-    ]
+        import pickle
+
+        with open(f"{path}/extracted_atomids.pi", "wb") as f:
+            pickle.dump(stored_atom_indices, f)
+    else:
+        stored_atom_indices = np.load(f"{path}/extracted_atomids.pi", allow_pickle=True)
 
     # Prepare sliced snapshots
     import mdtraj as md
@@ -245,27 +251,78 @@ def slice_snapshot(snapshot, project, run):
     return sliced_snapshot
 
 
+def find_lowest_reverse_work(project, run):
+    import glob
+    import re
+    import pandas as pd
+
+    clone = None
+    gen = None
+    lowest_work_value = 1e6
+    all_results_files = glob.glob(
+        f"/home/server/server2/data/SVR314342810/PROJ13422/RUN{run}/CLO*/res*/*csv"
+    )
+    for res in all_results_files:
+        with open(res, "rt") as infile:
+            df = pd.read_csv(infile)
+        # Drop duplicates
+        df.drop_duplicates(inplace=True)
+        protocol_work = df["protocol_work"].to_numpy()
+        if len(protocol_work) != 41:
+            break
+        r_work = protocol_work[40] - protocol_work[30]
+        ## get the reverse work
+        if r_work < lowest_work_value:
+            lowest_work_value = r_work
+            match = re.search(
+                "(?P<PROJ>PROJ\d+)/(?P<RUN>RUN\d+)/(?P<CLONE>CLONE\d+)/results(?P<GEN>\d+)/globals.csv$",
+                res,
+            )
+            clone = match.group("CLONE")
+            gen = match.group("GEN")
+    if clone is None or gen is None:
+        return None, None
+    return int(clone[5:]), int(gen)
+
+
 if __name__ == "__main__":
     # Example:
 
     # Define the snapshot to be extracted
-    project, run, clone, gen, frame, fragment_id = 13422, 0, 0, 0, 2, "x10789"
+    # frame 0 - old after EQ
+    # old -> new NEQ
+    # frame 1 - new after NEQ
+    # new EQ
+    # frame 2 - new after EQ
+    # new -> old NEQ
+    # frame 3 - old after NEQ
+    for run in range(352, 1000):
+        print(f"RUN: {run}")
+        project = 13422
+        fragment_id = "x10789"
+        frame = 3
 
-    # Extract the snapshots
-    sliced_snapshots, components = extract_snapshot(
-        project, run, clone, gen, frame, fragment_id=fragment_id
-    )
+        clone, gen = find_lowest_reverse_work(project, run)
+        print(clone, gen)
+        if clone is None or gen is None:
+            continue
 
-    # Write protein PDB
-    sliced_snapshots["protein"].save("protein.pdb")
+        # Extract the snapshots
+        # this could be looped over and the ligand positions clustered instead
+        sliced_snapshots, components = extract_snapshot(
+            project, run, clone, gen, frame, fragment_id=fragment_id
+        )
 
-    # Write old and new complex PDBs
-    for name in ["old_complex", "new_complex"]:
-        sliced_snapshots[name].save(f"{name}.pdb")
+        # Write protein PDB
+        sliced_snapshots["protein"].save(f"structures/RUN{run}-protein.pdb")
 
-    # Write ligand SDFs
-    from openeye import oechem
+        # Write old and new complex PDBs
+        for name in ["old_complex", "new_complex"]:
+            sliced_snapshots[name].save(f"structures/RUN{run}-{name}.pdb")
 
-    for name in ["old_ligand", "new_ligand"]:
-        with oechem.oemolostream(f"{name}.sdf") as ofs:
-            oechem.OEWriteMolecule(ofs, components[name])
+        # Write ligand SDFs
+        from openeye import oechem
+
+        for name in ["old_ligand", "new_ligand"]:
+            with oechem.oemolostream(f"structures/RUN{run}-{name}.sdf") as ofs:
+                oechem.OEWriteMolecule(ofs, components[name])
