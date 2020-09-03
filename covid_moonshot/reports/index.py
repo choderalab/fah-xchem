4ee45d87059817e425bf3e933cdc6c3dbe130b84
Matt Wittmann <mcwitt@gmail.com>
2020-09-03 16:49:14 -0700

    Add progress bar

--- covid_moonshot/reports/index.py
+++ covid_moonshot/reports/index.py
@@ -1,8 +1,7 @@
-from dataclasses import dataclass
 import importlib.resources as pkg_resources
 from math import floor, isfinite, log10
 import os
-from typing import List, Optional
+from typing import List, NamedTuple, Optional
 from jinja2 import Environment
 from ..analysis.constants import KT_KCALMOL
 from ..core import Analysis, Binding
@@ -13,8 +12,7 @@ from . import templates
 SPRINT_NUMBER = 3
 
 
-@dataclass
-class Estimate:
+class Estimate(NamedTuple):
     point: float
     stderr: float
 
@@ -47,6 +45,39 @@ def format_estimate_stderr(est: Estimate) -> str:
     return f"{round(est.stderr, prec):.{prec}f}"
 
 
+class Progress(NamedTuple):
+    completed: int
+    total: int
+
+    def percent_complete(self) -> float:
+        return 100 * self.completed / self.total
+
+
+# TODO: remove hardcoded values
+def _get_progress(
+    project: int = 13424, api_url: str = "http://aws3.foldingathome.org/api/"
+) -> Progress:
+    import requests
+    from simplejson.errors import JSONDecodeError
+
+    url = f"{api_url}/projects/{project}"
+
+    try:
+        response = requests.get(url=url)
+        json = response.json()
+    except (ConnectionError, JSONDecodeError) as exc:
+        raise RuntimeError(
+            f"Failed to get progress data from FAH server ({api_url})"
+        ) from exc
+
+    # TODO: remove hardcoded values
+    # override while we are still shifting WUs
+    n_gens = 2687 * 50 * 6  # sprint 3
+    # wus_total = json["gens"]
+
+    return Progress(completed=json["gens_completed"], total=n_gens,)
+
+
 def get_index_html(analysis: Analysis) -> str:
     template = pkg_resources.read_text(templates, "index.html")
     environment = Environment()
@@ -54,5 +85,5 @@ def get_index_html(analysis: Analysis) -> str:
     environment.filters["format_estimate_point"] = format_estimate_point
     environment.filters["format_estimate_stderr"] = format_estimate_stderr
     return environment.from_string(template).render(
-        sprint=SPRINT_NUMBER, analysis=analysis
+        sprint=SPRINT_NUMBER, analysis=analysis, progress=_get_progress(),
     )
