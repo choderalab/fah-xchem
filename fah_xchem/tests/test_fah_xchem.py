9796d19f774347fa88b8441d52a8edfa7f1af06a
Matt Wittmann <mcwitt@gmail.com>
2020-09-09 13:52:11 -0700

    Update test

--- fah_xchem/tests/test_fah_xchem.py
+++ fah_xchem/tests/test_fah_xchem.py
@@ -17,8 +17,9 @@ def compound_series():
             creator="John D. Chodera",
             creation_date=dt.datetime(2020, 9, 8, 10, 14, 48, 607238),
             xchem_project="Mpro",
-            biological_assembly="monomer",
-            protein_variant="thiolate",
+            receptor_variant=dict(
+                biological_assembly="monomer", protein_variant="thiolate"
+            ),
             temperature_kelvin=300,
             ionic_strength_millimolar=70,
             pH=7.4,
@@ -35,9 +36,6 @@ def compound_series():
             Compound(
                 compound_id="MAT-POS-f42f3716-2",
                 smiles="Cc1ccncc1NC(=O)Cc1cc(Cl)cc(-c2ccc(S(C)(=O)=O)cc2Cl)c1",
-                is_racemic_mixture=False,
-                has_multiple_protonation_states=False,
-                has_multiple_tautomers=False,
                 experimental_data={"pIC50": 4.324},
             ),
         ],
@@ -77,6 +75,6 @@ def test_fah_xchem_imported():
 
 def test_compound_series_json_serialization(compound_series):
     """Test json serialize/deserialize roundtrip for CompoundSeries"""
-    json = compound_series.to_json()
-    deserialized = CompoundSeries.from_json(json)
+    json = compound_series.json()
+    deserialized = CompoundSeries.parse_raw(json)
     assert deserialized == compound_series
