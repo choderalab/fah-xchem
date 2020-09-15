0f25f68689808910d6c6f6a475d70ed9939d9bd7
Matt Wittmann <mcwitt@gmail.com>
2020-09-15 13:27:32 -0700

    Fix typo

--- fah_xchem/analysis/exceptions.py
+++ fah_xchem/analysis/exceptions.py
@@ -7,7 +7,7 @@ class AnalysisError(RuntimeError):
 
 class DataValidationError(AnalysisError):
     def __init__(self, message: str, path: str):
-        super(message)
+        super().__init__(message)
         self.path = path
 
 
