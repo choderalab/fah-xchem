e3b26b4b65c7db8ea4dc4e049513199d6c8472ba
David Dotson <dotsdl@gmail.com>
2021-06-22 08:37:21 -0700

    Added initial test skeleton for structures

---
+++ fah_xchem/analysis/tests/test_structures.py
@@ -0,0 +1,16 @@
+import pytest
+
+from fah_xchem.analysis.structures import SnapshotArtifactory
+
+
+@pytest.fixture
+def snapshotartifactory():
+    pass
+
+
+class TestSnapshotArtifactory:
+    pass
+
+    def test_lol():
+        pass
+
