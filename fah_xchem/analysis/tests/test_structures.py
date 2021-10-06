cba25638c38fabd146a9d3ff5d3fca94255ec20a
David Dotson <dotsdl@gmail.com>
2021-10-05 22:20:05 -0700

    Added fixture for structure tests

--- fah_xchem/analysis/tests/test_structures.py
+++ fah_xchem/analysis/tests/test_structures.py
@@ -4,9 +4,15 @@ from fah_xchem.analysis.structures import SnapshotArtifactory
 
 
 @pytest.fixture
-def snapshotartifactory():
-    pass
+def snapshotartifactory(tmpdir):
+    with tmpdir.as_cwd():
 
+        waf = SnapshotArtifactory(
+                config=config,
+                project_dir=complex_project_dir,
+                project_data_dir=complex_data_dir,
+                cache_dir=cache_dir,
+            )
 
 class TestSnapshotArtifactory:
     pass
